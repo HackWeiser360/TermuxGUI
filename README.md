e89b279a62a9d024e84feb5c4b6cbca04ee1747a
MådMâx <74961526+HackWeiser360@users.noreply.github.com>
2020-12-20 16:48:19 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# TermuxGUI
+# Termux-GUI
+Use Termux in GUI mode using XFCE environment
+
+
 A simple python script to install XFCE environment in Termux
+
+## Installation
+1) apt install python3
+
+2) git clone https://github.com/HackWeiser360/TermuxGUI
+
+3) cd TermuxGUI
+
+4) pip3 install -r requirements.txt
+
+5) python3 gui.py
+
+## Download the VNC Viewer app
+VNC Viewer - [Download](https://play.google.com/store/apps/details?id=com.realvnc.viewer.android&hl=en_IN)
+
+## How to start Termux-GUI
+
+a) Run 'vncserver' command
+
+b) Add new password for vnc
+
+c) You will get IP Address like this -
+
+      localhost:<session_number>
+
+      e.g. localhost:1
+   
+d) After above step,type following command -
+
+      DISPLAY=:1 startxfce4 &
+
+   Here,1 is a session number.
+      
+e) Install VNC Viewer on your phone
+
+f) Add IP Address which you got at Step 3
+
+      e.g. localhost:1
+    
+g) Add name
+
+h) Click on Connect
+
+i) Enter VNC password which you used at Step 2
+
+## Exit GUI
+
+Enter following command before exit -
+
+            vncserver -kill :<session_number>
+            e.g. vncserver -kill :1
---
+++ gui.py
@@ -0,0 +1,73 @@
+import os
+import urllib.request
+import requests
+import sys
+
+try:
+    def con():
+        try:
+            print(G+"\nChecking internet connection........\n")
+            requests.get("http://google.com")
+        except requests.ConnectionError:
+            print(Y+"""
+<------------------------------>"""+R+"""
+Failed to connect to internet!\nCheck your internet connection :( """+Y+"""
+<-------------------------------->\n"""+W)
+            sys.exit()
+	        
+    R='\033[91m' #Red
+    Y='\033[93m' #Yellow
+    G='\033[92m' #Green
+    CY='\033[96m' #Cyan
+    W='\033[97m' #White
+    B='\033[95m' #Blue
+
+    print(Y+"""
+<----------------------------------------------->"""+G+"""
+              Termux GUI Installer"""+Y+"""
+<----------------------------------------------->"""+R+"""
+              == """+CY+"""By MådMâx """+R+"""==
+    
+""")
+    con()
+    print(Y+"\n==>"+CY+" Installing repositories............\n"+W)
+    os.system("apt install x11-repo")
+    os.system("apt install unstable-repo")
+    print(Y+"\n==>"+CY+" Installing XFCE Environment...........\n"+W)
+    os.system("apt install xfce xfce4-terminal tigervnc -y")
+    print(R+"""\n
+----------------------------------------------\n"""+R+"""
+== """+Y+"""Installation complete! """+R+"""=="""+CY+"""\n
+Now follow these steps to run Termux-GUI -->
+
+"""+R+"""1)"""+Y+""" Run """+G+"""'vncserver'"""+Y+""" command
+"""+R+"""2)"""+Y+""" Add new password for vnc
+"""+R+"""3)"""+Y+""" You will get IP Address like this -"""+G+"""
+
+   localhost:<session_number>"""+Y+"""
+   e.g. localhost:1
+   
+"""+R+"""3)"""+Y+""" After above step,type following command -"""+G+"""
+
+    DISPLAY=:1 startxfce4 &"""+Y+"""
+
+Here,1 is a session number.
+"""+R+"""4)"""+Y+""" Install VNC Viewer on your phone
+"""+R+"""5)"""+Y+""" Add IP Address which you got at Step 3"""+G+"""
+
+    e.g. localhost:1
+    
+"""+R+"""6)"""+Y+""" Add name
+"""+R+"""7)"""+Y+""" Click on Connect
+"""+R+"""8)"""+Y+""" Enter VNC password which you used at Step 2
+
+"""+G+"""
+** Important Note - If you want to exit from GUI,type following command before exit -
+
+-> vncserver -kill :<session_number>
+
+e.g. vncserver -kill :1
+"""+W)
+except KeyboardInterrupt:
+    print("Interrupted ! Good Bye! :) ")
+
